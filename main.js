6f1a34fba2dd53c08e813d84138b95e38e475d8c
Andrew Ledvina <wvvwwvw@gmail.com>
2017-02-24 14:18:12 -0800

    config

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 
 var rollbarConfig = {
-  accessToken: "12c99de67a444c229fca100e0967486f",
+  accessToken: "POST_CLIENT_ACCESS_TOKEN",
   ignoredMessages: ["Can't find Clippy.bmp. The end is nigh."],
   captureUncaught: true,
   captureUnhandledRejections: false,
